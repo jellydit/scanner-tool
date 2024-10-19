799b1ec341e80bbb3e2d280224f35883d1a46c92
JellyDonuts <admin@jellydit.com>
2024-10-19 13:16:50 -0700

    main, ver, vars

---
+++ terraform/accounts/main/us-gov-west-1/services/ecs/01/main.tf
@@ -0,0 +1,5 @@
+module "ecs_question_3" {
+  source = "../../../../../../modules/ecs-v0"
+
+  module_required_vars_here
+}
---
+++ terraform/modules/ecs-v0/variables.tf
@@ -0,0 +1,2 @@
+ALL OF THE VARIABLES FOR TWEAKING
+- some defaulted, some not (mandatory definitions needed in service)
